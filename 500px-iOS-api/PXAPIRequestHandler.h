19f7b66c73c9db9c473533df20d69a06bc2ee489
Ash Furrow <ash@500px.com>
2012-07-27 19:17:31 -0400

    Created request handler

---
+++ 500px-iOS-api/PXAPIRequestHandler.h
@@ -0,0 +1,13 @@
+//
+//  PXAPIRequestHandler.h
+//  500px-iOS-api
+//
+//  Created by Ash Furrow on 12-07-27.
+//  Copyright (c) 2012 500px. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PXAPIRequestHandler : NSObject
+
+@end

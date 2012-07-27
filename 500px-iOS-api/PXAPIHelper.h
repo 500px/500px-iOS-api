5c3fb9eaa0083d6c6fb00dfdde9a273536377103
Ash Furrow <ash@500px.com>
2012-07-27 18:37:10 -0400

    First commit. Created project.

--- 500px-iOS-api/PXAPIHelper.h
+++ 500px-iOS-api/PXAPIHelper.h
@@ -1,13 +1,13 @@
 //
-//  _00px_iOS_api.m
+//  PXAPIHelper.h
 //  500px-iOS-api
 //
 //  Created by Ash Furrow on 12-07-27.
 //  Copyright (c) 2012 500px. All rights reserved.
 //
 
-#import "_00px_iOS_api.h"
+#import <Foundation/Foundation.h>
 
-@implementation _00px_iOS_api
+@interface PXAPIHelper : NSObject
 
 @end
--- 500px-iOS-api/PXAPIHelper.m
+++ 500px-iOS-api/PXAPIHelper.m
@@ -1,13 +1,13 @@
 //
-//  _00px_iOS_api.h
+//  PXAPIHelper.m
 //  500px-iOS-api
 //
 //  Created by Ash Furrow on 12-07-27.
 //  Copyright (c) 2012 500px. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "PXAPIHelper.h"
 
-@interface _00px_iOS_api : NSObject
+@implementation PXAPIHelper
 
 @end

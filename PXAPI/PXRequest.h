258fa386efd233336406982487411b6592306a1f
Ash Furrow <ash@500px.com>
2012-08-10 11:26:35 -0400

    Initial work on PXRequest class.

---
+++ PXAPI/PXRequest.h
@@ -0,0 +1,14 @@
+//
+//  PXRequest.h
+//  PXAPI
+//
+//  Created by Ash Furrow on 2012-08-10.
+//  Copyright (c) 2012 500px. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PXRequest : NSObject
+
+
+@end
